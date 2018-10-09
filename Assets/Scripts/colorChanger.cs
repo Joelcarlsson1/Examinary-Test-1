abff0fbca1b73ad79536207d3b48cb613907043b
LEARNET\joel.carlsson1 <joel.carlsson@elev.ga.lbs.se>
2018-10-09 10:49:12 +0200

    Finished project

---
+++ Assets/Scripts/colorChanger.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour
+{
+    //The ship sprite variable
+    public SpriteRenderer shipRenderer;
+    //Decides if the transparency should go up or down
+    bool colorPulse;
+    //The transparency of the ship
+    public float transparency;
+    //The rgb values for the ship
+    float red;
+    float blue;
+    float green;
+    // Use this for initialization
+    void Start()
+    {
+        //Makes it so transparency will decrease in the start
+        colorPulse = true;
+        //Sets the transparency to full in the start
+        transparency = 1;
+        //Makes it so the ship is white in the beginning
+        red = 1;
+        green = 1;
+        blue = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Changes the ships color each frame according the specified rgba values
+        shipRenderer.color = new Color(red, green, blue, transparency);
+        //If colorPulse is true the transperancy gets reduced every frame. If the transperancy is below 0.5 it turns of colorPulse
+            if (colorPulse == true)
+        {
+            transparency = transparency - 0.01f;
+            if (transparency <= 0.5)
+            {
+                colorPulse = false;
+            }
+        }
+        //If colorPulse is false the transparency gets increased every frame until it is 1 then it switches colorPulse to false
+        if (colorPulse == false)
+        {
+            transparency = transparency + 0.01f;
+            if (transparency >= 1)
+            {
+                colorPulse = true;
+            }
+
+        }
+
+        //Makes it so when you hold "D" red and green get the value 0 and blue gets 1. So that blue is the only color
+        if (Input.GetKey(KeyCode.D))
+        {
+            red = 0;
+            green = 0;
+            blue = 1;
+        }
+        //Makes it so when you hold "A" red and blue get the value 0 and green gets 1. So that green is the only color
+        if (Input.GetKey(KeyCode.A))
+        {
+            red = 0;
+            green = 1;
+            blue = 0;
+        }
+        //When Space bar is pressed the ship gets a random color
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            red = Random.Range(0f, 1f);
+            green = Random.Range(0f, 1f);
+            blue = Random.Range(0f, 1f);
+        }
+
+
+
+
+    }
+}
--- Assets/Scripts/shipMovement.cs
+++ Assets/Scripts/shipMovement.cs
@@ -8,42 +8,97 @@ public class shipMovement : MonoBehaviour
     float leftRotationSpeed;
     //The ships base turn speed
     public float baseRotationSpeed;
+    //How much slower left rotation is than the right rotation
+    [Range(0.1f, 1f)]
+    public float leftSlowerRatio;
     //Player assigned ship speed
     public float baseSpeed;
     //The current ship speed
     float forwardSpeed;
-    //The ship sprite variable
-    public SpriteRenderer shipRenderer;
+    //Lets the players decide if they want to have random speed or not
+    public bool randomSpeed = true;
+    //Lets the player decide if they want a random start position
+    public bool randomStartPos = true;
+    //Variables for each ship type
+    public SpriteRenderer ship1;
+    public SpriteRenderer ship2;
+    public SpriteRenderer ship3;
+    //Variable for getting a random ship number
+    int randomShip;
     //The amount of time while in game
-    public int timer;
-    
-    
-    
+    int timer;
+    //The current location of the ship sprite
+    public Transform shipLocation;
+
     // Use this for initialization
     void Start()
     {
+        
+        //If randomSpeed is true the base ship speed gets randomized
+        //The base ship speed gets randomized at the start of the game      
+        if (randomSpeed == true)
+        {
+            baseSpeed = Random.Range(1, 11);
+        }
+        //If randomStartPos is true the ship gets a random position within the camera
+        if (randomStartPos == true)
+        {
+            shipLocation.position = new Vector3(Random.Range(-8f, 8f), Random.Range(-4.4f, 4.4f));
+        }
         //Sets the ship's current speed to the player assigned ship speed
         forwardSpeed = baseSpeed;
-        leftRotationSpeed = baseRotationSpeed / 2;
+        //Sets the left rotation to be slower based on the leftSlowerRatio
+        leftRotationSpeed = baseRotationSpeed * leftSlowerRatio;
+        //Resets the timer so that it counts normally each time you play
+        timer = 1;
+        //Sets each spriterenderer to false
+        ship1.enabled = false;
+        ship2.enabled = false;
+        ship3.enabled = false;
+        //Picks a number bewtween 1 and 3
+        //Depending on the number one of the ships gets their spriterenderer enabled
+        randomShip = Random.Range(1, 4);
+        if (randomShip == 1)
+        {
+            ship1.enabled = true;
+        }
+        if (randomShip == 2)
+        {
+            ship2.enabled = true;
+        }
+        if (randomShip == 3)
+        {
+            ship3.enabled = true;
+        }
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         //Makes the ship go forward all the time based on the given speed, in real time
         transform.Translate(forwardSpeed * Time.deltaTime, 0f, 0f, Space.Self);
-        //Makes it so when you hold "D" the ship rotates to the right and the ship turns blue
-        if (Input.GetKey(KeyCode.D))
-        {       
-                transform.Rotate(0f, 0f, -baseRotationSpeed * Time.deltaTime);
-            
-            shipRenderer.color = new Color(0f, 0f, 1f, 1f);
+        //Makes it so when you hold "W" the ship speed doubles
+        if (Input.GetKey(KeyCode.W))
+        {
+            forwardSpeed = baseSpeed * 2;
         }
-        //Makes it so when you hold "A" the ship rotates to the left and the ship turns green
+        //When you release W the speed resets
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            forwardSpeed = baseSpeed;
+        }
+        //Makes it so when you hold "D" the ship rotates to the right independant of framerate
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, -baseRotationSpeed * Time.deltaTime);
+        }
+        //Makes it so when you hold "A" the ship rotates to the left independant of framerate
+        //The rotation is based on the leftSlowerRatio
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0f, 0f, leftRotationSpeed * Time.deltaTime);
-            shipRenderer.color = new Color(0f, 1f, 0f, 1f);
         }
         //Makes it so when you hold "S" the ship speed halfs
         if (Input.GetKey(KeyCode.S))
@@ -55,15 +110,29 @@ public class shipMovement : MonoBehaviour
         {
             forwardSpeed = baseSpeed;
         }
+
         //Prints the current time spent in game each second. 
         //It does this by checking if the time spent in game is equal to the value of the timer variable
         //If it is then it prints the value of timer and adds 1 to it. So that it is executed again in the next second
-      
         if (Time.fixedTime == timer)
         {
             print("Timer: " + timer);
             timer = timer + 1;
         }
+
+
+
+        //If the x of the rocket is below or above +- 10.5 it is teleported to the oppiosite side
+        if (transform.position.x < -10.5 || transform.position.x > 10.5)
+        {
+            shipLocation.position = new Vector3(shipLocation.position.x * -1f, shipLocation.position.y);
+        }
+        //If the y of the rocket is below or above +- 10.5 it is teleported to the oppiosite side
+        if (transform.position.y > 6.5 || transform.position.y < -6.5)
+        {
+            shipLocation.position = new Vector3(shipLocation.position.x, shipLocation.position.y * -1f);
+        }
+
     }
-   
+
 }
